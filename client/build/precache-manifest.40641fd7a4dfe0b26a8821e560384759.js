4a81c30b814d1b8ae46d93df57d5c7e202bf8f5b
Arthur <arthur@strzewiczek.pl>
2020-07-29 13:39:25 +0200

    Add files via upload

---
+++ client/build/precache-manifest.40641fd7a4dfe0b26a8821e560384759.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0fb203e92badf68ba1b52d73539766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5d8abd2b50f6fadcb0",
+    "url": "/static/css/main.a5ebd15a.chunk.css"
+  },
+  {
+    "revision": "c26fc67a3cd902a3166b",
+    "url": "/static/js/2.29f17bab.chunk.js"
+  },
+  {
+    "revision": "8a5d8abd2b50f6fadcb0",
+    "url": "/static/js/main.3f0bc0a1.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
